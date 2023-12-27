5bb2f8d67a49b5c3440666416cb50f2d8f74cc98
Sammuel Espiritu <108394895+smml-espiritu@users.noreply.github.com>
2023-12-27 12:58:01 +0800

    feat(ui): add about page

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout";
+import About from "@/components/about";
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <About />
+    </Layout>
+  );
+};
+
+export default AboutPage;
