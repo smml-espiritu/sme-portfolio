d1678e936f36e3dc4e4499be2fb4884d2610502d
Sammuel Espiritu <108394895+smml-espiritu@users.noreply.github.com>
2023-12-27 13:42:36 +0800

    feat(ui): add project page

---
+++ src/pages/project/index.tsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/layout";
+import Projects from "@/components/project";
+import React from "react";
+
+const ProjectPage = () => {
+  return (
+    <Layout>
+      <Projects />
+    </Layout>
+  );
+};
+
+export default ProjectPage;
