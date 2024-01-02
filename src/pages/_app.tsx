5896a487bc5c6a2393de80a95c265ff02992196d
Sammuel Espiritu <108394895+smml-espiritu@users.noreply.github.com>
2024-01-02 14:38:41 +0800

    update to the meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,17 +7,43 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>SME | Front-end Dev.</title>
-        <meta name="description" content="My portfolio website" />
+        {/* HTML Meta Tags */}
+        <meta
+          name="description"
+          content="Explore my diverse portfolio showcasing a collection of my creative works and projects."
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://sme-portfolio.vercel.app/" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content="SME | Front-end Dev." />
         <meta
           property="og:description"
           content="Explore my diverse portfolio showcasing a collection of my creative works and projects."
         />
-        <meta property="og:url" content="https://sme-portfolio.vercel.app" />
         <meta
           property="og:image"
-          content="https://sme-portfolio.vercel.app/assets/meta-image.png"
+          content="https://opengraph.b-cdn.net/production/documents/6c008fbb-3df2-41a1-9183-2c54ff8b4b61.png?token=MfztP2NVnm3PafD0fsLxb-vpv9flIbESIiPolaCy0Ws&height=340&width=577&expires=33240177403"
         />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="sme-portfolio.vercel.app" />
+        <meta
+          property="twitter:url"
+          content="https://sme-portfolio.vercel.app/"
+        />
+        <meta name="twitter:title" content="SME | Front-end Dev." />
+        <meta
+          name="twitter:description"
+          content="Explore my diverse portfolio showcasing a collection of my creative works and projects."
+        />
+        <meta
+          name="twitter:image"
+          content="https://opengraph.b-cdn.net/production/documents/6c008fbb-3df2-41a1-9183-2c54ff8b4b61.png?token=MfztP2NVnm3PafD0fsLxb-vpv9flIbESIiPolaCy0Ws&height=340&width=577&expires=33240177403"
+        />
+
+        {/* Meta Tags Generated via https://www.opengraph.xyz */}
       </Head>
       <Component {...pageProps} />
     </>
