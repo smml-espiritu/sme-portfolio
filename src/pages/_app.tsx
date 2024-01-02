e6a45cb5c723ab7e4727a780f9746b3ca37495a1
Sammuel Espiritu <108394895+smml-espiritu@users.noreply.github.com>
2024-01-02 13:04:24 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import MetaImage from "../../public/assets/projects/Phone.png";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -14,10 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
           property="og:description"
           content="Explore my diverse portfolio showcasing a collection of my creative works and projects."
         />
-        <meta
-          property="og:url"
-          content="https://sme-portfolio.vercel.app/assets/projects/Phone.png"
-        />
+        <meta property="og:url" content="https://sme-portfolio.vercel.app" />
         <meta
           property="og:image"
           content={`https://sme-portfolio.vercel.app/assets/projects/Phone.png`}
